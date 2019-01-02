9be9fb896ff04a68d07b4f6e4f1cae640274d505
robertsteele <steele.210@osu.edu>
2019-01-02 13:50:37 -0500

    3.1 Used apache common lang for Char conversion

--- src/main/java/ToDoApp.java
+++ src/main/java/ToDoApp.java
@@ -1,8 +1,10 @@
+import org.apache.commons.lang3.CharUtils;
 import utils.CommandLineInput;
 import utils.CommandLineInputHandler;
+import org.apache.commons.lang3.CharUtils.*;
 
 public class ToDoApp {
-    public static final char DEFAULT_INPUT = '\u0000';
+    private static final char DEFAULT_INPUT = '\u0000';
 
     public static void main(String args[]) {
         CommandLineInputHandler commandLineInputHandler = new CommandLineInputHandler();
@@ -11,8 +13,7 @@ public class ToDoApp {
         while (CommandLineInput.EXIT.getShortCmd() != command) {
             commandLineInputHandler.printOptions();
             String input = commandLineInputHandler.readInput();
-            char[] inputChars = input.length() == 1 ? input.toCharArray() : new char[]{DEFAULT_INPUT};
-            command = inputChars[0];
+            command = CharUtils.toChar(input, DEFAULT_INPUT);
             CommandLineInput commandLineInput = CommandLineInput.getCommandLineInputForInput(command);
             commandLineInputHandler.processInput(commandLineInput);
         }
